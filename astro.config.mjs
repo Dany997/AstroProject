84eabb23a76e971462bd6c0603f1787579c0ab28
Dhuzio <dany97@o2.pl>
2025-03-16 22:45:56 +0100

    another last chance lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
+// import { defineConfig } from 'astro/config';
+// import node from '@astrojs/node';
+
+// export default defineConfig({
+// 	output: 'server', // 👈 Przełącz Astro na tryb SSR
+// 	adapter: node({ mode: 'standalone' }),
+// });
+
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 
 export default defineConfig({
-	output: 'server', // 👈 Przełącz Astro na tryb SSR
-	adapter: node({ mode: 'standalone' }),
+	output: 'static', // 👈 Przełącza Astro na statyczny build
 });
